60617fd9f66149c1cdf67a7f62513af81f695ce1
Ronan <ronanmingon69550@gmail.com>
2022-12-09 07:02:29 +0100

    add traffic light front

--- client/src/app/traffic-light/traffic-light.component.ts
+++ client/src/app/traffic-light/traffic-light.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {TrafficLightEvent} from "../models/trafficLightType";
 
 @Component({
   selector: 'app-traffic-light',
   template: `<div class="flex flex-col">
-      <span class="bg-red-500 h-4 w-4 rounded-full"></span>
-      <span class="bg-green-500 h-4 w-4 rounded-full"></span>
+      <span class="bg-red-500 h-4 w-4 rounded-full" (click)="setRed()"></span>
+      <span class="bg-green-500 h-4 w-4 rounded-full" (click)="setGreen()"></span>
     </div>`
 })
 export class TrafficLightComponent {
+
+  @Input() number: number = 0
+  @Input() state: "red" | "green" = "red"
+  @Output() traffic = new EventEmitter<TrafficLightEvent>();
+
+  setRed() {
+    this.traffic.emit({state: "red", position: this.number})
+  }
+
+  setGreen() {
+    this.traffic.emit({state: "green", position: this.number})
+  }
 
 }
