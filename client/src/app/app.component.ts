a149f4290cee20b05e10db16309d600313634296
Ronan <ronanmingon69550@gmail.com>
2022-12-09 07:01:32 +0100

    add join

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WSService} from "./ws/ws.service";
+import {TrafficLightEvent} from "./models/trafficLightType";
+import {take} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   type = ""
   constructor(private readonly wsService: WSService) {}
 
+  ngOnInit(): void {
+
+  }
+
   newGame() {
-    this.wsService.subject.next({
+    this.wsService.ws.next({
       command: 'NEW_GAME',
       role: this.type
     })
+  }
+
+  join() {
+    this.wsService.wsSubject.next({
+      command: 'JOIN'
+    })
+  }
+
+  setTrafficEvent(event: TrafficLightEvent) {
+    this.wsService.wsSubject.next({
+      command: "COMMAND",
+      func: "setTrafficLight",
+      arg: event
+    })
+    console.log(event)
   }
 }
