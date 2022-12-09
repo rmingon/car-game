7a3dd9c4469c3a30f23d002b18cf19f362a03c6d
Ronan <ronanmingon69550@gmail.com>
2022-12-09 07:03:51 +0100

    abstract ws subject for surcharge

--- client/src/app/ws/ws.service.ts
+++ client/src/app/ws/ws.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
 import { webSocket } from "rxjs/webSocket";
+import {Subject} from "rxjs";
+
+interface Data {
+  id: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class WSService {
-  subject = webSocket("ws://localhost:8080");
+  ws = webSocket<object>("ws://localhost:8080");
+  wsSubject = new Subject<object>()
+  id: string = ""
 
   constructor() {
-    this.subject.subscribe((data) => console.log(data))
+    this.ws.subscribe((data) => {
+      console.log(data)
+      // @ts-ignore
+      if (data?.id) {
+        // @ts-ignore
+        this.id = data?.id
+      }
+    })
+    this.wsSubject.subscribe((data) => {
+      const dataWithId = {...data, id: this.id}
+      this.ws.next(dataWithId)
+    })
   }
 }
