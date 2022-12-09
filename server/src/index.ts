79e69c261db46b0b764e5eeb5849c8b5f023eab9
Ronan <ronanmingon69550@gmail.com>
2022-12-09 07:02:14 +0100

    add join and send to players

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,10 +8,18 @@ const party = new Map()
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(message) {
-    let data = JSON.parse(message.toString())
-    if (data.command === "NEW_GAME")
-      party.set(makeId(8), new Game(data.role, ws))
-    if (data.command === "COMMAND")
-      (party.get(data.id))[data.func](...data.arg)
-  });
+    try {
+      let data = JSON.parse(message.toString())
+      if (data.command === "NEW_GAME") {
+        const id = makeId(8)
+        party.set(id, new Game(data.role, ws, id))
+      }
+      if (data.command === "JOIN")
+        (party.get(data.id)).join(ws)
+      if (data.command === "COMMAND")
+        (party.get(data.id))[data.func](data.arg)
+    } catch (e) {
+      console.error(e)
+    }
+  })
 });
