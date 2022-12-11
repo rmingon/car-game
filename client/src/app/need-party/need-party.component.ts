84e0c55ee95360520298332a331a80f2b5662178
Ronan <ronanmingon69550@gmail.com>
2022-12-11 21:18:14 +0100

    add needs player subscribe

--- client/src/app/need-party/need-party.component.ts
+++ client/src/app/need-party/need-party.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import {WSService} from "../ws/ws.service";
-
+import {filter} from "rxjs";
+import {Need, Needs} from "../models/action";
 
 @Component({
   selector: 'app-need-party',
   templateUrl: './need-party.component.html'
 })
 export class NeedPartyComponent {
-  constructor(private readonly wsService: WSService) {
-    this.wsService.needs.subscribe(next => {
-      console.log(next)
-    })
+  needs: Need[] | undefined
+
+  constructor(public readonly wsService: WSService) {
+    this.wsService.ws
+    // @ts-ignore
+      .pipe(filter<Needs>((el) => el.type === "needs"))
+      .subscribe((next: Needs) => {
+        this.needs = next.needs
+      })
   }
 }
