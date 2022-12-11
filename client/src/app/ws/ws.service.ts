74f610c2d6c2c4c92a68bb160b77a5b739c7a1fd
Ronan <ronanmingon69550@gmail.com>
2022-12-11 15:54:18 +0100

    add filter observable

--- client/src/app/ws/ws.service.ts
+++ client/src/app/ws/ws.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { webSocket } from "rxjs/webSocket";
-import {Subject} from "rxjs";
+import {filter, map, Observable, Subject, Subscribable} from "rxjs";
 
 interface Data {
   id: string
+}
+
+interface Needs {
+  type: string,
+  needs: object[],
+  number_parties: number,
 }
 
 @Injectable({
@@ -14,15 +20,20 @@ export class WSService {
   wsSubject = new Subject<object>()
   id: string = ""
 
+  needs: Observable<object> // TODO type needs
+
   constructor() {
     this.ws.subscribe((data) => {
-      console.log(data)
+      // console.log(data)
       // @ts-ignore
       if (data?.id) {
         // @ts-ignore
         this.id = data?.id
       }
     })
+
+    this.needs = this.ws.pipe(filter(el => el.hasOwnProperty('needs')))
+
     this.wsSubject.subscribe((data) => {
       const dataWithId = {...data, id: this.id}
       this.ws.next(dataWithId)
