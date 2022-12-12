1c27b13e3ade6fd373e26040d789d57045c147b9
Ronan <ronanmingon69550@gmail.com>
2022-12-12 07:03:45 +0100

    add pipe logic with observer

--- client/src/app/ws/ws.service.ts
+++ client/src/app/ws/ws.service.ts
@@ -1,19 +1,48 @@
 import {Injectable, OnInit} from '@angular/core';
 import { webSocket } from "rxjs/webSocket";
 import {filter, map, Observable, pipe, Subject, Subscribable} from "rxjs";
-import {Id, Needs} from "../models/action";
+import {Cars, Connect, Messages, Needs, role} from "../models/action";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WSService implements OnInit {
   ws = webSocket<object>("ws://localhost:8080");
+
   id: string = ""
+  role: role | undefined
 
-  constructor() {}
+  cars: Observable<Cars> | undefined
 
-  join(id: string) {
+  messages: Observable<Messages> | undefined
+
+  constructor() {
+
+    this.ws.subscribe(next => {
+      console.log(next)
+    })
+
+    this.ws
+      // @ts-ignore
+      .pipe(filter<Connect>((el => el.type === 'id')))
+      .subscribe((next: Connect) => {
+        this.id = next.id
+        this.role = next.role
+      })
+
+    this.cars = this.ws
+      // @ts-ignore
+      .pipe(filter<Cars>(el => el.type==="cars"))
+
+    this.messages = this.ws
+      // @ts-ignore
+      .pipe(filter<Messages>(el => el.type==="msg"))
+
+  }
+
+  join(id: string, role: role) {
     this.id = id
+    this.role = role
     this.ws.next({
       command: 'JOIN',
       id
@@ -21,12 +50,7 @@ export class WSService implements OnInit {
   }
 
   ngOnInit(): void {
-    this.ws
-      // @ts-ignore
-      .pipe(filter<Id>((el => el.type === 'id')))
-      .subscribe((next: Id) => {
-        this.id = next.id
-    })
+
   }
 
   newGame(type: string) {
