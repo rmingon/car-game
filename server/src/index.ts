68e3a234c55d4c70b8e16799330c195e4556b494
Ronan <ronanmingon69550@gmail.com>
2022-12-14 06:31:42 +0100

    catch on msg error

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,13 +7,17 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(message) {
-    let data = JSON.parse(message.toString())
-    if (data.command === "NEW_GAME")
-      Game.newGame(data.role, ws)
-    if (data.command === "JOIN")
-      Game.join(data.id, ws)
-    if (data.command === "COMMAND")
-      Game.command(data.id, data.func, data.arg)
+    try {
+      let data = JSON.parse(message.toString())
+      if (data.command === "NEW_GAME")
+        Game.newGame(data.role, ws)
+      if (data.command === "JOIN")
+        Game.join(data.id, ws)
+      if (data.command === "COMMAND")
+        Game.command(data.id, data.func, data.arg)
+    } catch (e) {
+      ws.send(JSON.stringify({error: e}))
+    }
   })
 
   wss.clients.forEach(function each(client) {
