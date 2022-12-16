6b846b5006be6cbc60cae0fae30dca62df0f8594
Ronan <ronanmingon69550@gmail.com>
2022-12-16 07:02:35 +0100

    add area type

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,1 +1,8 @@
 export type direction = "top" | "right" | "bottom" | "left"
+
+export interface Area {
+  TL: [number, number],
+  TR: [number, number],
+  BL: [number, number],
+  BR: [number, number],
+}
